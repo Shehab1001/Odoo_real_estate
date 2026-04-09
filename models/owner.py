50f7dd5c3eb0b5752ff4da481fd43154303d687f
shehab1001 <shehab.eldin.said1@gmail.com>
2026-04-09 11:22:53 +0200

    adding unit tests + test_odoo.sh to run these tests

--- models/owner.py
+++ models/owner.py
@@ -3,8 +3,8 @@ from odoo import models, fields
 class Owner(models.Model):
     _name = "owner"
     _description = "Owner"
-    name = fields.Char(required=1)
+    name = fields.Char(required=True)
     address = fields.Char()
-    phone = fields.Char(required=1)
+    phone = fields.Char(required=True)
     property_ids = fields.One2many('property', 'owner_id')
 
